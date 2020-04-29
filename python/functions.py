67328ec66d7d21d54ebeb96b0b23d471a82ac4e4
Bartosz Hojnacki <bartosz.hojnacki@gmail.com>
2020-04-29 13:30:41 +0200

    dodano test porównania średnich oraz testy normalności rozkładu

--- python/functions.py
+++ python/functions.py
@@ -1,6 +1,10 @@
 
 import pandas as pd
 from matplotlib import pyplot as plt 
+import numpy as np 
+import scipy.stats as st
+
+
 
 class Datasets:
     patient = 'data/PatientInfo.csv'
@@ -29,4 +33,122 @@ def plot_hist(df,column=None):
     plt.hist(df[column])
     plt.title(column)
 
+
+
+## Funkcja porównująca średnie
+
+# zanim dodałem if - wskazując dwie te same kolumny dostawałem p-value = 0 czyli należałoby
+# uznać, że średnie w między kolumnami się różnią :P 
+
+
+def porownaj_srednie(x,y, a = 0.05):
+    
+    if x is y:
+        print('Należy wybrać dwie różne kolumny.')
+        
+    else:
+        
+        try:
+            mx = np.mean(x)
+            my = np.mean(y)
+            nx = len(x)
+            ny = len(y)
+
+            sx = np.std(x)
+            sy = np.std(y)
+
+            u = mx - my/np.sqrt(sx**2/nx + sy**2/ny)
+            
+            rozkladNormalny = st.norm()
+
+            pvalue = rozkladNormalny.cdf(u)
+
+
+            print('Wartość p-value {} jest {} niż wartość a {}'.format(pvalue, 'większa' if pvalue >a else 'mniejsza', a))
+
+            if pvalue > a:
+                  print('Nie ma podstaw do odrzucenia hipotezy zerowej')
+            else:
+                  print('Istnieją przesłanki do odrzucenia hipotezy zerowej na rzecz alternatywnej - średnia mx jest niższa od średniej my')
+
+
+        except TypeError:
+            print('Zmienne w podanych kolumnach muszą mieć charakter numeryczny')
+
+
+
+            
+        except:
+            print('Wystąpił nieoczekiwany błąd')
+
+
+
+
+##Test normalności rozkładu
+
+def sprawdz_rozklad(x):
+    #x.dropna(inplace=True) #konieczne jest usunięcie nanów by f-cja działała
+    N = len(x)
+    
+    try:
+        if N <= 100:
+            #Test Shapiro-Wilka
+            st.shapiro(x)
+            test = st.shapiro(x)[0]
+            pvalue = st.shapiro(x)[1]
+            
+            print('Statystyka testowa = {}, p-value = {}, wartość p-value jest {} od statystyki testowej'.format(test, pvalue, 'większa' if pvalue > test else 'mniejsza'))
+
+            if pvalue > test:
+                print('Wartość pvalue jest wyższa od statystyki testowej - można uznać, że próba podchodzi z rozkładu normalnego')
+
+            else:
+                print('Wartość pvalue jest niższa od statystyki testowej - nie można uznać, że próba pochodzi z rozkładu normalnego')
+
+            
+            
+        else:
+            #Test Kołomogowa-Smirnova
+            #Ewentualnie zamiast rysowania tutaj wykresu można przywołać zdefiniowaną funkcję
+                      
+            
+            x = np.array(x)
+            x = x.reshape(N,)
+
+            #plt.hist(x, bins = 25, edgecolor = 'black')
+            
+            mx = np.mean(x)
+            sx = np.std(x)
+            
+            rozkladNormalny = st.norm(mx,sx)
+            
+            xsorted = np.sort(x)
+            
+            F = rozkladNormalny.cdf(xsorted)
+            
+            Fni = np.fromiter(range(1, N+1), dtype =int)/N
+            
+            #wykres dystrybuanty
+            plt.plot(xsorted,F,color="black")
+            plt.scatter(xsorted, Fni, color="red")
+            plt.legend(["F(x)","Fni"])
+            
+            #obliczanie wartości
+            rozkladKS = st.kstwobign()
+
+            Dn = max(np.abs(F - Fni))
+            stTest = np.sqrt(N)*Dn
+            pvalue = 1 - rozkladKS.cdf(stTest)
+
+            print('Dn = {}, p-value = {}, wartość p-value jest {} od Dn'.format(Dn, pvalue, 'większa' if pvalue > Dn else 'mniejsza'))
+
+            if pvalue > Dn:
+                print('Wartość pvalue jest wyższa od statystyki testowej Dn - można uznać, że próba podchodzi z rozkładu normalnego')
+
+            else:
+                print('Wartość pvalue jest niższa od statystyki Dn - nie można uznać, że próba pochodzi z rozkładu normalnego')
+
+
+    except TypeError:
+        print('Zmienne we wskazanej kolumnie muszą mieć charakter numeryczny.')
 
